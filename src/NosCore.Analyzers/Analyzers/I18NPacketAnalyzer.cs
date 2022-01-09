a0dfd2e7c71b0bef4e9dccecd6f92d4c7ea1fca0
erwan-joly <erwan.joly@pushpay.com>
2022-01-09 18:52:14 +1300

    I18NPacketAnalyzer analyzer

---
+++ src/NosCore.Analyzers/Analyzers/I18NPacketAnalyzer.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.FindSymbols;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace NosCore.Analyzers.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class I18NPacketAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "NosCoreAnalyzers";
+
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.Resources.AnalyzerTitle), Resources.Resources.ResourceManager, typeof(Resources.Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.Resources.AnalyzerMessageFormat), Resources.Resources.ResourceManager, typeof(Resources.Resources));
+        private const string Category = "Naming";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(AnalyseInvocation, SyntaxKind.ObjectCreationExpression);
+        }
+
+        private static void AnalyseInvocation(SyntaxNodeAnalysisContext context)
+        {
+            if (context.Node is not ObjectCreationExpressionSyntax node)
+            {
+                return;
+            }
+
+            var type = context.SemanticModel.GetTypeInfo(node).Type;
+            var properties = type.GetMembers().Where(m => m.Kind == SymbolKind.Property).OfType<IPropertySymbol>().ToList();
+
+            var enumProperty = properties?.FirstOrDefault(x => x.Type.Name == "Game18NConstString");
+            if (enumProperty == null)
+            {
+                return;
+            }
+
+            var argumentProperty = properties?.FirstOrDefault(x => x.Type.TypeKind == TypeKind.Array && (x.Type as IArrayTypeSymbol)?.ElementType.Name == nameof(Object));
+            if (argumentProperty == null)
+            {
+                return;
+            }
+
+
+            var assignments = node.Initializer.Expressions.OfType<AssignmentExpressionSyntax>().ToList();
+            var enumAssignment = assignments.FirstOrDefault(x => (x?.Left as IdentifierNameSyntax)?.Identifier.Value.ToString() == enumProperty.Name);
+            var argumentAssignment = assignments.FirstOrDefault(x => (x?.Left as IdentifierNameSyntax)?.Identifier.Value.ToString() == argumentProperty.Name);
+
+            var arguments = new List<string>();
+            if (enumAssignment != null)
+            {
+                var enumValue = (enumAssignment.Right as MemberAccessExpressionSyntax);
+                var enumType = context.SemanticModel.GetTypeInfo(enumValue).Type;
+                var fields = enumType.GetMembers().Where(m => m.Kind == SymbolKind.Field).OfType<IFieldSymbol>().ToList();
+                var field = fields.FirstOrDefault(x => x.Name == enumValue?.Name.ToString()) ?? fields.First();
+                var attribute = field.GetAttributes().FirstOrDefault(x => x.AttributeClass.Name == "Game18NArgumentsAttribute");
+                arguments.AddRange(attribute.ConstructorArguments.First().Values.Select(x => x.Value.ToString()));
+            }
+
+            var error = false;
+            if (argumentAssignment != null)
+            {
+                var expressions = (argumentAssignment.Right as ArrayCreationExpressionSyntax)?.Initializer.Expressions.ToList() ?? new List<ExpressionSyntax>();
+
+                if (arguments.Count != expressions.Count)
+                {
+                    error = true;
+                }
+                else
+                {
+                    for (var index = 0; index < arguments.Count; index++)
+                    {
+                        var expression = expressions[index];
+                        var argument = arguments[index];
+                        var literal = expression as LiteralExpressionSyntax;
+                        var variable = context.SemanticModel.GetTypeInfo(expression).Type;
+                        bool? isValid;
+                        switch (argument)
+                        {
+                            case "string":
+                                isValid = literal?.IsKind(SyntaxKind.StringLiteralExpression)
+                                          ?? variable.Name == nameof(String);
+                                if (isValid != true)
+                                {
+                                    error = true;
+                                }
+
+                                break;
+
+                            case "long":
+                                isValid = literal?.IsKind(SyntaxKind.NumericLiteralExpression)
+                                          ?? variable.Name is nameof(Int32) or nameof(Int16) or nameof(Int64);
+                                if (isValid != true)
+                                {
+                                    error = true;
+                                }
+
+                                break;
+
+                            default:
+                                error = true;
+                                break;
+                        }
+                    }
+                }
+            }
+            if (error)
+            {
+                var diagnostic = Diagnostic.Create(Rule, node.GetLocation(), string.Join(",", arguments));
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
---
+++ test/NosCore.Analyzers.Tests/NosCoreAnalyzersUnitTests.cs
@@ -0,0 +1,115 @@
+﻿using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VerifyCS = NosCore.Analyzers.Test.CSharpAnalyzerVerifier<
+    NosCore.Analyzers.Analyzers.I18NPacketAnalyzer>;
+
+namespace NosCore.Analyzers.Tests
+{
+    [TestClass]
+    public class NosCoreAnalyzersUnitTest
+    {
+        public readonly string TestClass = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace NosCore.Example
+    {{
+
+        [AttributeUsage(AttributeTargets.Field)]
+        public class Game18NArgumentsAttribute : Attribute
+        {{
+            public Game18NArgumentsAttribute(params Type[] argumentTypes)
+            {{
+                ArgumentTypes = argumentTypes;
+            }}
+
+            public Type[] ArgumentTypes {{ get; set; }}
+        }}
+
+        public enum Game18NConstString
+        {{
+            [Game18NArguments(typeof(long), typeof(string))]
+            Test
+        }}
+
+        public class Packet {{
+            public Game18NConstString Message {{ get; set; }}
+
+            public object[] Game18NArguments {{ get; set; }}
+        }}
+
+        public class TestObj {{
+
+            public TestObj(int testInt, string testString) {{
+                TestInt = testInt;
+                TestString = testString;
+            }}
+            public int TestInt {{ get; set; }}
+            public string TestString {{ get; set; }}
+        }}
+
+        public class Program
+        {{ 
+            public void Main() 
+            {{
+                var testString = ""testString"";
+                var testInt = 123;
+                var testObj = new TestObj(123, ""testString"");
+                var test = new Packet
+                {{
+                    Message = Game18NConstString.Test,
+                    Game18NArguments = new object[] {{ {0} }}
+                }};
+            }}
+        }}
+    }}";
+
+        //No diagnostics expected to show up
+        [TestMethod]
+        public async Task TestWithWrongArgumentCount()
+        {
+            var test = string.Format(TestClass, "123");
+
+            var result = VerifyCS.Diagnostic().WithSpan(52, 28, 56, 18).WithArguments("long,string");
+            await VerifyCS.VerifyAnalyzerAsync(test, result);
+        }
+
+
+        [TestMethod]
+        public async Task TestWithRightArgumentCount()
+        {
+            var test = string.Format(TestClass, "123, \"Gold\"");
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task TestWithRightArgumentCountButWrongType()
+        {
+            var test = string.Format(TestClass, "\"test\", \"Gold\"");
+
+            var result = VerifyCS.Diagnostic().WithSpan(52, 28, 56, 18).WithArguments("long,string");
+            await VerifyCS.VerifyAnalyzerAsync(test, result);
+        }
+
+        [TestMethod]
+        public async Task TestWithVariables()
+        {
+            var test = string.Format(TestClass, "testInt, testString");
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task TestWithProperties()
+        {
+            var test = string.Format(TestClass, "testObj.TestInt, testObj.TestString");
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+    }
+}
